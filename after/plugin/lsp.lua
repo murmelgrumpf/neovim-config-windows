42398a7b47c5b39948815a79412f21269d00da5d
murmelgrumpf <murmelgrumpf@aol.com>
2024-01-03 10:34:48 +0100

    Changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,7 +18,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'eslint', 'rust_analyzer'},
+  ensure_installed = {'eslint', 'rust_analyzer', 'gopls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
