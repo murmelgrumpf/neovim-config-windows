c357987906e9c438b16501a9e27131aeab438934
murmelgrumpf <murmelgrumpf@aol.com>
2024-01-10 22:05:49 +0100

    nother one

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -16,53 +16,56 @@ vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-y>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 
-vim.api.nvim_create_autocmd({"BufEnter"},
-{
-    pattern = {"*.ts", "*.hbs"},
-    callback = function()
-        path = normalize_path( 
-            vim.api.nvim_buf_get_name(
-                vim.api.nvim_get_current_buf()
-            ),
-            vim.loop.cwd()
-        )
-        if string.find(path, "\\templates\\") or string.find(path, "\\routes\\") or string.find(path, "\\controllers\\") then
-            list = harpoon:list()
-            list:clear()
-            genericPath = path:gsub("\\templates\\", "\\COOLTEMPPATHTHINGY\\"):gsub("%.hbs", "FILEEND")
-            genericPath = genericPath:gsub("\\routes\\", "\\COOLTEMPPATHTHINGY\\"):gsub("%.ts", "FILEEND")
-            genericPath = genericPath:gsub("\\controllers\\", "\\COOLTEMPPATHTHINGY\\")
-            list:append(list.config.create_list_item(list.config, genericPath:gsub("COOLTEMPPATHTHINGY", "controllers"):gsub("FILEEND", ".ts"))) 
-            list:append(list.config.create_list_item(list.config, genericPath:gsub("COOLTEMPPATHTHINGY", "templates"):gsub("FILEEND", ".hbs"))) 
-            list:append(list.config.create_list_item(list.config, genericPath:gsub("COOLTEMPPATHTHINGY", "routes"):gsub("FILEEND", ".ts"))) 
-            harpoon:sync()
-        end
-        if string.find(path, "\\components\\") then
-            list = harpoon:list()
-            list:clear()
-            genericPath = path:gsub("%.ts", "FILEEND"):gsub("%.hbs", "FILEEND")
-            list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", ".ts"))) 
-            list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", ".hbs"))) 
-            harpoon:sync()
-        end
-    end,
-})
+vim.api.nvim_create_autocmd({ "BufEnter" },
+    {
+        pattern = { "*.ts", "*.hbs" },
+        callback = function()
+            local path = normalize_path(
+                vim.api.nvim_buf_get_name(
+                    vim.api.nvim_get_current_buf()
+                ),
+                vim.loop.cwd()
+            )
+            if string.find(path, "\\templates\\") or string.find(path, "\\routes\\") or string.find(path, "\\controllers\\") then
+                local list = harpoon:list()
+                list:clear()
+                local genericPath = path:gsub("\\templates\\", "\\COOLTEMPPATHTHINGY\\"):gsub("%.hbs", "FILEEND")
+                genericPath = genericPath:gsub("\\routes\\", "\\COOLTEMPPATHTHINGY\\"):gsub("%.ts", "FILEEND")
+                genericPath = genericPath:gsub("\\controllers\\", "\\COOLTEMPPATHTHINGY\\")
+                list:append(list.config.create_list_item(list.config,
+                    genericPath:gsub("COOLTEMPPATHTHINGY", "controllers"):gsub("FILEEND", ".ts")))
+                list:append(list.config.create_list_item(list.config,
+                    genericPath:gsub("COOLTEMPPATHTHINGY", "templates"):gsub("FILEEND", ".hbs")))
+                list:append(list.config.create_list_item(list.config,
+                    genericPath:gsub("COOLTEMPPATHTHINGY", "routes"):gsub("FILEEND", ".ts")))
+                harpoon:sync()
+            end
+            if string.find(path, "\\components\\") then
+                local list = harpoon:list()
+                list:clear()
+                local genericPath = path:gsub("%.ts", "FILEEND"):gsub("%.hbs", "FILEEND")
+                list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", ".ts")))
+                list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", ".hbs")))
+                harpoon:sync()
+            end
+        end,
+    })
 
-vim.api.nvim_create_autocmd({"BufEnter"},
-{
-    pattern = {"de.json", "en.json"},
-    callback = function()
-        path = normalize_path( 
-            vim.api.nvim_buf_get_name(
-                vim.api.nvim_get_current_buf()
-            ),
-            vim.loop.cwd()
-        )
-        list = harpoon:list()
-        list:clear()
-        genericPath = path:gsub("de.json", "FILEEND"):gsub("en.json", "FILEEND")
-        list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", "de.json"))) 
-        list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", "en.json"))) 
-        harpoon:sync()
-    end,
-})
+vim.api.nvim_create_autocmd({ "BufEnter" },
+    {
+        pattern = { "de.json", "en.json" },
+        callback = function()
+            local path = normalize_path(
+                vim.api.nvim_buf_get_name(
+                    vim.api.nvim_get_current_buf()
+                ),
+                vim.loop.cwd()
+            )
+            local list = harpoon:list()
+            list:clear()
+            local genericPath = path:gsub("de.json", "FILEEND"):gsub("en.json", "FILEEND")
+            list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", "de.json")))
+            list:append(list.config.create_list_item(list.config, genericPath:gsub("FILEEND", "en.json")))
+            harpoon:sync()
+        end,
+    })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "C:\\Users\\jngipper\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1703942320\\share\\lua\\5.1\\?.lua;C:\\Users\\jngipper\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1703942320\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\jngipper\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1703942320\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\jngipper\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1703942320\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
-local install_cpath_pattern = "C:\\Users\\jngipper\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1703942320\\lib\\lua\\5.1\\?.so"
+local package_path_str = "C:\\Users\\MURMEL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1696883897\\share\\lua\\5.1\\?.lua;C:\\Users\\MURMEL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1696883897\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\MURMEL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1696883897\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\MURMEL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1696883897\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
+local install_cpath_pattern = "C:\\Users\\MURMEL~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.1696883897\\lib\\lua\\5.1\\?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,135 +76,135 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["auto-session"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\auto-session",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\auto-session",
     url = "https://github.com/rmagatti/auto-session"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lua",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-path",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp_luasnip",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   dracula = {
     config = { "\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme dracula\bcmd\bvim\0" },
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\dracula",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\dracula",
     url = "https://github.com/Mofiqul/dracula.nvim"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\friendly-snippets",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["git-blame.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\git-blame.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\git-blame.nvim",
     url = "https://github.com/f-person/git-blame.nvim"
   },
   harpoon = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\harpoon",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lsp-zero.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lualine.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason-lspconfig.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason-null-ls.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason-null-ls.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason-null-ls.nvim",
     url = "https://github.com/jay-babu/mason-null-ls.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["none-ls.nvim"] = {
     config = { "\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fnull-ls\frequire\0" },
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\none-ls.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\none-ls.nvim",
     url = "https://github.com/nvimtools/none-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\nvim-web-devicons",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\undotree",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "C:\\Users\\jngipper\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
+    path = "C:\\Users\\murmelgrumpf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   }
 }
